086e41b473aaaf6e99e3f0c7a4699c35c272b6bb
shehan weerasekara <tharindushehan732@gmail.com>
2024-11-15 15:03:18 +0530

    practises

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [Count,setCount] = useState(0);
+
+  const decrement = () => {
+    setCount(Count - 1)
+  }
+  const increment = () => {
+    setCount(Count + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Count</h1>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+      <h3>{Count}</h3>
+    </>
   );
 }
 
